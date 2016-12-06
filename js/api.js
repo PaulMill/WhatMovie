ba286773a204edaa009f1e07d6a2cf675d171e76
PaulMill <usa.paul@icloud.com>
2016-12-05 22:40:57 -0800

    #1 created button "Movie by Year"

---
+++ js/api.js
@@ -0,0 +1,11 @@
+const url_now_release = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_date=2016-09-15&primary_release_date=2016-12-3`;
+
+const url_release2015_2016 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_date=2015-01-01&primary_release_date=2016-12-31`;
+
+const url_release2010_2014 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_year=2010&primary_release_year=2014`;
+
+const url_release2000_2009 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_year=2000&primary_release_year=2009`;
+
+const url_release1990_1999 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_year=1990&primary_release_year=1999`;
+
+const url_release1970_1989 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_year=1970&primary_release_year=1989`;
---
+++ js/main.js
@@ -0,0 +1,149 @@
+(function() {
+  'use strict';
+  //for using side-bar
+  // $('.button-collapse').sideNav({
+  //      menuWidth: 200, // Default is 240
+  //      edge: 'right', // Choose the horizontal origin
+  //    }
+  //  );
+
+  let searchResult = [];
+
+  const createCard = () => {
+    $('#cards').empty();
+    const $container = $('<div>').attr({id:'cards'}).addClass('container row');
+    $('main').append($container);
+
+    for (const card of searchResult) {
+
+      const $cardCol = $('<section>').addClass('col s12 m6');
+      const $headerCard = $('<h5>').addClass('header pink-text center-align').text(card.title);
+      const $cardPos = $('<div>').addClass('card horizontal card-panel hoverable');
+
+      const $cardImage = $('<div>').addClass('card-image');
+      const $image = $('<img>').attr({
+        src:`https://image.tmdb.org/t/p/w500${card.poster_path}`,
+        alt:`Poster ${card.poster_path}`
+      });
+
+      $cardImage.append($image);
+
+      const $cardStacked = $('<div>').addClass('card-stacked');
+      const $cardContent = $('<div>').addClass('card-content');
+      const $descr = $('<div>').attr({id:'bold'}).text("Movie description:");
+      const $quote = $('<blockquote>').text(card.overview);
+      const $release = $('<div>').attr({id:'bold'}).text(`Release date: ${card.release_date}`);
+      const $fullDescr = $('<div>').attr({id:'more_info'});
+      const $moreInfo = $('<a>').addClass('waves-effect waves-green btn-flat').attr({http: `${$modal}`}).text('More info...');
+
+      $fullDescr.append($moreInfo);
+      $cardContent.append($descr, $quote, $release, $fullDescr);
+
+      var $modal = $('<div>').addClass('modal modal-content').attr({id:'modal1'});
+      const $par = $('<p>').text(card.overview);
+      $modal.append($descr, $par);
+
+      const $cardAction = $('<div>').addClass('card-action');
+      const $button1 = $('<a>').attr({
+        href:`http://youtube.com`,
+        id:`YTLink`
+      }).text('trailer');
+      const $button2 = $('<a>').attr({
+        href:`http://google.com/`,
+        id:`GoogLink`
+      }).text('watch');
+
+      $cardAction.append($button1, $button2);
+      $cardStacked.append($cardContent, $cardAction);
+      $cardPos.append($cardImage, $cardStacked);
+      $cardCol.append($headerCard, $cardPos);
+      $container.append($cardCol);
+    };
+  };
+
+  const popByYear = () => {
+    // const years = [];
+    const years = ["Just Released", [2016, 2015], [2014, 2010], [2009, 2000], [1999, 1990], [1989, 1970]];
+// creating and adding div with container of buttons "years"
+    const $container = $('<div>').attr({id:'buttons'}).addClass('container row');
+    $('main').append($container);
+
+// iterraiting through years to adding HTML and creating cards
+    for (const card of years) {
+      const $cardCol = $('<div>').addClass('col s6 l3 offset-l1');
+      const $cardContent = $('<div>').addClass('card-panel grey lighten-1');
+      const $aContent = $('<a>').addClass('white-text a-big').attr({href: "#cards"});
+
+// checking if array of years contains strings and adding to .text
+      if (card === "Just Released") {
+        $aContent.addClass('new-release').text(card);
+      } else
+      {
+        $aContent.addClass(`button${card[1]}`).text(`${card[1]} - ${card[0]}`);
+      }
+
+// appending to DOM
+      $cardContent.append($aContent);
+      $cardCol.append($cardContent);
+      $container.append($cardCol);
+    }
+
+// call buttons Movie by Year function
+    $('.new-release').on("click", (event) => {
+      getDataAPI(url_now_release);
+    });
+    $('.button2015').on("click", (event) => {
+      getDataAPI(url_release2015_2016);
+    });
+    $('.button2010').on('click', (event) => {
+      getDataAPI(url_release2010_2014);
+    });
+    $('.button2000').on('click', (event) => {
+      getDataAPI(url_release2000_2009);
+    });
+    $('.button1990').on('click', (event) => {
+      getDataAPI(url_release1990_1999);
+    });
+    $('.button1970').on('click', (event) => {
+      getDataAPI(url_release1970_1989);
+    });
+  };
+
+//button Popular by year
+  $('#movieYear').on('click', (event) => {
+    event.preventDefault();
+    popByYear(event);
+  });
+//button on the nav-bar
+
+  $('.new-release').on('click', (event) => {
+    getDataAPI(url_now_release);
+  });
+
+
+  function getDataAPI(api_url) {
+    const $xhr = $.ajax({
+      method: 'GET',
+      url: api_url,
+      dataType: 'json'
+    });
+
+    $xhr.done((data) => {
+      if ($xhr.status !== 200) {
+        return;
+      }
+      console.log(data);
+      for (let i = 0; i < 8; i++) {
+        searchResult.push(data.results[i]);
+      }
+      createCard(searchResult);
+        // $('#moreMovie').on('click', (event) => {
+        //   let moreMov = [];
+        //   moreMov = searchResult.splice(0, 8);
+        //   createCard(searchResult);
+        //   console.log(moreMov);
+        // });
+      console.log(searchResult);
+    });
+  };
+})();
