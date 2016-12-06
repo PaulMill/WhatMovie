e9febd4503fb70b4e533bfd1443244250cb62c53
PaulMill <usa.paul@icloud.com>
2016-12-06 12:06:07 -0800

    #1 created button "Movie by Actor"

--- js/api.js
+++ js/api.js
@@ -1,11 +1,128 @@
-const url_now_release = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_date=2016-09-15&primary_release_date=2016-12-3`;
+function urlApiMovie(data1, data2) {
+  return `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_year=${data1}&primary_release_year=${data2}&sort_by=popularity.desc`;
+}
 
-const url_release2015_2016 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_date=2015-01-01&primary_release_date=2016-12-31`;
+const url_now_release = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_date=2016-09-15&primary_release_date=2016-12-3&sort_by=popularity.desc`;
 
-const url_release2010_2014 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_year=2010&primary_release_year=2014`;
-
-const url_release2000_2009 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_year=2000&primary_release_year=2009`;
-
-const url_release1990_1999 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_year=1990&primary_release_year=1999`;
-
-const url_release1970_1989 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&primary_release_year=1970&primary_release_year=1989`;
+const actors = [
+  {
+    name: "Will Smith",
+    id: 2888,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=2888&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/2iYXDlCvLyVO49louRyDDXagZ0G.jpg`
+  },
+  {
+    name: "Robert De Niro",
+    id: 380,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=380&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/8Bgdfv1oN9Mw0YuMHP6fw8KzDkc.jpg`
+  },
+  {
+    name: "Tom Hanks",
+    id: 31,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=31&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/a14CNByTYALAPSGlwlmfHILpEIW.jpg`
+  },
+  {
+    name: "Brad Pitt",
+    id: 287,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=287&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/kc3M04QQAuZ9woUvH3Ju5T7ZqG5.jpg`
+  },
+  {
+    name: "Denzel Washington",
+    id: 5292,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=5292&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/t9arcZbg1nLt8GZt2SkWm227YoK.jpg`
+  },
+  {
+    name: "Leonardo DiCaprio",
+    id: 6193,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=6193&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/A85WIRIKVsD2DeUSc8wQ4fOKc4e.jpg`
+  },
+  {
+    name: "Nicolas Cage",
+    id: 2963,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=2963&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/fW37Gbk5PJZuXvyZwtcr0cMwPKY.jpg`
+  },
+  {
+    name: "Colin Firth",
+    id: 5472,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=5472&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/kbs5HzE2KjzbKiGYQw2aXFpdvaX.jpg`
+  },
+  {
+    name: "George Clooney",
+    id: 1461,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=1461&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/z4SBfpKDThuQY0FsvDbajcooBdA.jpg`
+  },
+  {
+    name: "Jason Statham",
+    id: 976,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=976&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/PhWiWgasncGWD9LdbsGcmxkV4r.jpg`
+  },
+  {
+    name: "Julia Roberts",
+    id: 1204,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=1204&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/yzaIyUEKHSnEYDwltXs8gpF4SVC.jpg`
+  },
+  {
+    name: "Angelina Jolie",
+    id: 11701,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=11701&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/wdrSStdTmiSHhynCV5CtTJTuzY1.jpg`
+  },
+  {
+    name: "Jennifer Aniston",
+    id: 4491,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=4491&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/uH9FwXtRpiboKeqHUk0RuJRAm6I.jpg`
+  },
+  {
+    name: "Cameron Diaz",
+    id: 6941,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=6941&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/ahFkUN9Sm8oF1txUHE5JcJ95Ere.jpg`
+  },
+  {
+    name: "Kate Hudson",
+    id: 11661,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=11661&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/gGMM8T4QAd3lFC3CURMsmlVH37O.jpg`
+  },
+  {
+    name: "Sandra Bullock",
+    id: 18277,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=18277&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/bsAy8f8UZKairXQzRukU5FP4XAQ.jpg`
+  },
+  {
+    name: "Charlize Therson",
+    id: 6885,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=6885&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/fG0mtmBm3OsvKFucvoQyqBnVwya.jpg`
+  },
+  {
+    name: "Sarah Jessica Parker",
+    id: 520,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=520&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/u2fVzLtsqtDVUSXCkVpdDdUoy48.jpg`
+  },
+  {
+    name: "Natalie Portman",
+    id: 524,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=524&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/jJcRWku3e9OHrmRqytn6WcBjhvh.jpg`
+  },
+  {
+    name: "Kate Winslet",
+    id: 204,
+    url_api: `https://api.themoviedb.org/3/discover/movie?api_key=a7a6367c472249d9e3def2c5b9bb046e&with_cast=204&sort_by=popularity.desc`,
+    url_img: `https://image.tmdb.org/t/p/w500/w8wjPbS24vPErNeYhAvtbyAUBMd.jpg`
+  },
+];
--- js/main.js
+++ js/main.js
@@ -30,9 +30,9 @@
 
       const $cardStacked = $('<div>').addClass('card-stacked');
       const $cardContent = $('<div>').addClass('card-content');
-      const $descr = $('<div>').attr({id:'bold'}).text("Movie description:");
+      const $descr = $('<div>').addClass('bold-text').text(`Movie description:`);
       const $quote = $('<blockquote>').text(card.overview);
-      const $release = $('<div>').attr({id:'bold'}).text(`Release date: ${card.release_date}`);
+      const $release = $('<div>').addClass('bold-text').text(`Release date: ${card.release_date}`);
       const $fullDescr = $('<div>').attr({id:'more_info'});
       const $moreInfo = $('<a>').addClass('waves-effect waves-green btn-flat').attr({http: `${$modal}`}).text('More info...');
 
@@ -62,7 +62,6 @@
   };
 
   const popByYear = () => {
-    // const years = [];
     const years = ["Just Released", [2016, 2015], [2014, 2010], [2009, 2000], [1999, 1990], [1989, 1970]];
 // creating and adding div with container of buttons "years"
     const $container = $('<div>').attr({id:'buttons'}).addClass('container row');
@@ -86,40 +85,86 @@
       $cardContent.append($aContent);
       $cardCol.append($cardContent);
       $container.append($cardCol);
+      $('.new-release').on('click', (event) => {
+        getDataAPI(url_now_release);
+      });
+      $(`.button${card[1]}`).on('click', (event) =>{
+        getDataAPI(urlApiMovie(card[1], card[0]));
+      });
     }
 
 // call buttons Movie by Year function
-    $('.new-release').on("click", (event) => {
-      getDataAPI(url_now_release);
-    });
-    $('.button2015').on("click", (event) => {
-      getDataAPI(url_release2015_2016);
-    });
-    $('.button2010').on('click', (event) => {
-      getDataAPI(url_release2010_2014);
-    });
-    $('.button2000').on('click', (event) => {
-      getDataAPI(url_release2000_2009);
-    });
-    $('.button1990').on('click', (event) => {
-      getDataAPI(url_release1990_1999);
-    });
-    $('.button1970').on('click', (event) => {
-      getDataAPI(url_release1970_1989);
-    });
+    // $('.new-release').on('click', (event) => {
+    //   getDataAPI(url_now_release);
+    // });
+    // $('.button2015').on('click', (event) => {
+    //   getDataAPI(url_release2015_2016);
+    // });
+    // $('.button2010').on('click', (event) => {
+    //   getDataAPI(url_release2010_2014);
+    // });
+    // $('.button2000').on('click', (event) => {
+    //   getDataAPI(url_release2000_2009);
+    // });
+    // $('.button1990').on('click', (event) => {
+    //   getDataAPI(url_release1990_1999);
+    // });
+    // $('.button1970').on('click', (event) => {
+    //   getDataAPI(url_release1970_1989);
+    // });
   };
-
 //button Popular by year
   $('#movieYear').on('click', (event) => {
     event.preventDefault();
     popByYear(event);
   });
-//button on the nav-bar
 
+  const popByActor = () => {
+
+// creating and adding div with container of buttons "actors"
+    const $container = $('<div>').attr({id:'actorCollection'}).addClass('container row');
+    $('main').append($container);
+
+// iterraiting through actor file to adding HTML and creating collection
+    for (const card of actors) {
+      const $collection = $('<ul>').addClass('collection col s6 l3 offset-l1');
+
+      $container.append($collection);
+      const $colItem = $('<li>').addClass('collection-item avatar');
+
+      $collection.append($colItem);
+      const $imgActor = $('<img>').attr({
+        src: card.url_img,
+        alt: `Picture of ${card.name}`
+      }).addClass('circle');
+      const $titleActor = $('<span>').addClass('title bold-text').text(card.name);
+      const $p1 = $('<p>').text('popularity: ');
+      const $p2 = $('<p>').text('details: ');
+      const $a = $('<a>').addClass(`secondary-content button${card.id}`).attr({href: "#cards"})
+      const $icon = $('<i>').addClass('material-icons pink-text').text('thumb_up');
+
+// appending to DOM
+      $a.append($icon);
+      $colItem.append($imgActor, $titleActor, $p1, $p2, $a);
+
+// creating click event for each actor
+      $(`.button${card.id}`).on('click', (event) =>{
+        getDataAPI(card.url_api);
+      });
+    }
+
+  }
+  // adding eventListener for button "Movie by actor"
+  $('#movieActor').on('click', (event) => {
+    event.preventDefault();
+    popByActor(event);
+  });
+
+
+//button on the nav-bar
   $('.new-release').on('click', (event) => {
     getDataAPI(url_now_release);
   });
-
 
   function getDataAPI(api_url) {
     const $xhr = $.ajax({
